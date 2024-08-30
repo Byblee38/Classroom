bfcf5526afb182931324934166c74025d98b9de4
Bayu Kresna Mukti <kresnamukti386@gmail.com>
2024-08-30 23:51:01 +0700

    update tanggal 30 Agustus 2024

--- Classroom/src/Page/Header_and_Footer/Header.jsx
+++ Classroom/src/Page/Header_and_Footer/Header.jsx
@@ -1,8 +1,18 @@
 import style from './Header.module.scss';
 import logo_kelas from '../../assets/Image/Header_and_Footer/logo_kelas.png';
 import nama_kelas from '../../assets/Image/Header_and_Footer/nama_kelas.png';
+import {useState} from 'react';
+import {Bars3Icon} from '@heroicons/react/24/outline';
+import {Drawer} from '@mui/material';
+import {useEffect} from 'react';
 
-function header () {
+function Header () {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => setIsOpen((pervState) => !pervState);
+    useEffect(() => {
+        alert('Web nya masih dalam taham pengembangan, harap bersabar!');
+    }, []);
+
     return (
         <header className={style.header}>
             <div className={style.logo}>
@@ -12,16 +22,33 @@ function header () {
             <div className={style.space}></div>
             <div className={style.menu}>
                 <ul className={style.list}>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Academics</li>
-                    <li>Admissions</li>
-                    <li>Student Life</li>
-                    <li>Contact</li>
+                    <li className={style.listH}>Home</li>
+                    <li className={style.listH}>About Us</li>
+                    <li className={style.listH}>Academics</li>
+                    <li className={style.listH}>Admissions</li>
+                    <li className={style.listH}>Student Life</li>
+                    <li className={style.listH}>Contact</li>
                 </ul>
+            </div>
+            <div className={style.drawer}>
+                <Bars3Icon className={style.icon} onClick={handleClick}/>
+                <Drawer
+                anchor="right"
+                open={isOpen}
+                onClose={handleClick}
+                >
+                    <ul style={{listStyle: 'none', padding: '1rem', fontFamily: 'Outfit', cursor: 'pointer', fontSize: '14px', backgroundColor: '#f0f5ff', width: '100px', marginTop: '0px', height: '100vh', marginBottom: '0px'}}>
+                        <li style={{marginBottom: '10px'}}>Home</li>
+                        <li style={{marginBottom: '10px'}}>About Us</li>
+                        <li style={{marginBottom: '10px'}}>Academic</li>
+                        <li style={{marginBottom: '10px'}}>Admissions</li>
+                        <li style={{marginBottom: '10px'}}>Student Life</li>
+                        <li style={{marginBottom: '10px'}}>Contact</li>
+                    </ul>
+                </Drawer>
             </div>
         </header>
     ) 
 }
 
-export default header;
+export default Header;
--- Classroom/src/Page/Home/Component/Hero/Hero.jsx
+++ Classroom/src/Page/Home/Component/Hero/Hero.jsx
@@ -2,13 +2,18 @@ import style from "./Hero.module.scss";
 import hero_image from "../../../../assets/Image/Hero/image_hero.png";
 
 function Hero() {
+
+  
   return (
     <div className={style.hero}>
       <img src={hero_image} alt="Foto_Kelas" className={style.image} />
       <div className={style.hero_text}>
         <p className={style.welcome}>Welcome to Little Learners Academy</p>
         <h1 className={style.headingtext}>
-          Where Young Minds Blossom <br /> and <span style={{ color: "#4d59ff", fontSize: "40px"}}>Dreams Take Flight</span>
+          Where Young Minds Blossom <br /> and{" "}
+          <span style={{ color: "#4d59ff", fontSize: "40px"}}>
+            Dreams Take Flight
+          </span>
         </h1>
         <p className={style.paragraph}>
           Our kinder garden school provides a nurturing and stimulating
@@ -19,7 +24,7 @@ function Hero() {
           <div className={style.information}>
             <h1 className={style.number}>+7000</h1>
             <p className={style.textnumber}>Students Passed Out</p>
-            </div>
+          </div>
           <div className={style.information}>
             <h1 className={style.number}>+37</h1>
             <p className={style.textnumber}>Awards & Recognitions</p>
