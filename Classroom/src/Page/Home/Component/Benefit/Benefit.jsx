e94f2c9828afe3a5b84db6cbf1856754ad4c2f1f
Bayu Kresna Mukti <kresnamukti386@gmail.com>
2024-09-01 10:30:38 +0700

    Update tanggal 01 September 2024

--- Classroom/src/Page/Home/Component/Benefit/Benefit.jsx
+++ Classroom/src/Page/Home/Component/Benefit/Benefit.jsx
@@ -8,22 +8,24 @@ function Benefit() {
         <div className={style.chip}>
           <h1 className={style.textchip}>Children Deserve Bright Future</h1>
         </div>
-        <h1>Our Benefits</h1>
-        <p>
+        <h1 className={style.header}>Our Benefits</h1>
+        <p style={{ textAlign: "center" }}>
           With a dedicated team of experienced educators, state-of-the-art
           facilities, and a comprehensive curriculum, we aim to lay a strong
           foundation for your child future.
         </p>
       </div>
+      <div className={style.card}>
       {Benefitcard.list.map((item) => (
         <div className={style.benecard} key={item.id}>
           <div className={style.beneicon}><img className={style.logoicon} src={item.icon} alt="icon" /></div>
           <div className={style.information}>
-            <h1>{item.title}</h1>
-            <p>{item.paragraph}</p>
+            <h1 className={style.texttitle}>{item.title}</h1>
+            <p className={style.textparagraf}>{item.paragraph}</p>
           </div>
         </div>
       ))}
+      </div>
     </div>
   );
 }
